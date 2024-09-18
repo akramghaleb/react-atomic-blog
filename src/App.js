24b4ab7f9d957573972c63fc11b49f81cb377da3
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-09-18 10:59:43 +0300

    A Surprising Optimization Trick With children

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { PostProvider, usePosts, createRandomPost } from "./PostProvider";
-
+import Test from "./Test";
 function App() {
   const [isFakeDark, setIsFakeDark] = useState(false);
 
@@ -113,14 +113,16 @@ function FormAddPost() {
 function List() {
   const { posts } = usePosts()
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
